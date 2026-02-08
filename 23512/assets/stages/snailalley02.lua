8edadec2c4e380110c11febbb8373640dc6626df
shayderrr <pranavbajjuri@gmail.com>
2026-02-08 15:34:19 -0600

    a

--- 23512/assets/stages/snailalley02.lua
+++
@@ -1,9 +0,0 @@
-function onCreate()
-	-- background shit
-	makeLuaSprite('snailalley02', 'snailalley02', -600, -300);
-	setScrollFactor('snailalley02', 0.9, 0.9);
-
-	addLuaSprite('snailalley02', false);
-
-	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
-end
